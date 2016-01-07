ef2e65c8fbfed449d35d55cb951a354d7e4a0283
Tahrim <utahrim@gmail.com>
2016-01-07 16:12:58 -0500

    scrolls to the right sections

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-  $("#right_nav a").on("click", function(){
+  $("#right_nav_about a").on("click", function(){
+    $('body').animate({
+      scrollTop: $(".photo").offset().top}, 1000, 'swing');
+  });
+    $("#right_nav_projects a").on("click", function(){
+    $('body').animate({
+      scrollTop: $(".photo1").offset().top}, 1000, 'swing');
+  });
+      $("#right_nav_contact a").on("click", function(){
     $('body').animate({
       scrollTop: $(".photo2").offset().top}, 1000, 'swing');
   });
