7e955138aefd739d8243bef3f7bcfe30763b7d14
Tahrim <utahrim@gmail.com>
2016-01-12 05:06:04 -0500

    add new jquery effect

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -17,8 +17,7 @@ $(document).ready(function(){
 
   document.body.style.overflow = 'hidden'
 
-  $(".planet").planetarium({
-  });
+  $(".planet").planetarium();
 
   $(window).resize(function() {
     if ($(window).width() >= 1349 && $(window).width() < 1372) {
@@ -37,4 +36,13 @@ $(document).ready(function(){
       $(".earth.planet").hide();
     };
   })
+
+// $(".about_me").flipping_text({
+//     tickerTime: 7, // Define a time between each ticket in milliseconds. The default value is 10.
+//     customRandomChar: false, // You can use your own random strings by defining them here. The default value is false which will use my random string: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+//     tickerCount: 5, // Set the number of characters randomly shown before the real text is shown here. The default value is 10.
+//     opacityEffect: true, // You can toggle the opacity effect here. Set this to false if you don't want the random text to fade in. The default value is 10.
+//     resetOnChange: true // Toggle this to true if you want the plugin to stop and fill in all the text immediately when the user changes browser's tab. The default value is false.
+//   });
+
 });
